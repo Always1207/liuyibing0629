8ba377ab34c42f6782db3ebe45597d394bcd373f
Always1207 <1404218203@qq.com>
2022-05-16 22:22:34 +0800

    lab1

---
+++ lab1/src/com/liuyibing/controller/ProductDetailsServlet.java
@@ -0,0 +1,52 @@
+package com.liuyibing.controller;
+
+import com.liuyibing.dao.ProductDao;
+import com.liuyibing.model.Category;
+import com.liuyibing.model.Product;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebServlet(name = "ProductDetailsServlet", value = "/productDetails")
+public class ProductDetailsServlet extends HttpServlet {
+    Connection con=null;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        con=(Connection) getServletContext().getAttribute("con");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            List<Category> categoryList = Category.findAllCategory(con);
+            request.setAttribute("categoryList", categoryList);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        try {
+        if (request.getParameter("id")!=null) {
+            int productId = Integer.parseInt(request.getParameter("id"));
+            ProductDao productDao = new ProductDao();
+            Product product = productDao.findById(productId, con);
+            request.setAttribute("p",product);
+        }
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+
+        String path ="/WEB-INF/views/productDetails.jsp";
+        request.getRequestDispatcher(path).forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ lab1/src/com/liuyibing/controller/ShopServlet.java
@@ -0,0 +1,57 @@
+package com.liuyibing.controller;
+
+import com.liuyibing.dao.ProductDao;
+import com.liuyibing.model.Category;
+import com.liuyibing.model.Product;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebServlet(name = "ShopServlet", value = "/shop")
+public class ShopServlet extends HttpServlet {
+    Connection con=null;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        con=(Connection) getServletContext().getAttribute("con");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        try {
+            List<Category> categoryList = Category.findAllCategory(con);
+            request.setAttribute("categoryList", categoryList);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+            ProductDao productDao=new ProductDao();
+            try {
+                if (request.getParameter("categoryId") == null) {
+                    List<Product> productList = productDao.findAll(con);
+                    request.setAttribute("productList",productList);
+                } else {
+                    int catId = Integer.parseInt(request.getParameter("categoryId"));
+                    List<Product> productList = productDao.findByCategoryId(catId,con);
+                    request.setAttribute("productList",productList);
+                }
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+
+            String path ="/WEB-INF/views/shop.jsp";
+            request.getRequestDispatcher(path).forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
